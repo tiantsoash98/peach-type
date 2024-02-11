79f96365a7dda023f65daaba9df6679a34ad922d
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-02-11 19:41:36 +0300

    Responsive text

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,27 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      title: 'Peach Fuzz Challenge',
+      htmlAttrs: {
+        lang: 'fr',
+        prefix: 'og: https://ogp.me/ns#'
+      },
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/svg',
+          href: '/favicon.svg'
+        }
+      ]
+    }
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="h-screen">
+    <div class="h-dvh">
         <div class="h-full w-full max-w-4xl mx-auto flex flex-col justify-center">
             <div class="w-full relative">
+                <div class="absolute z-10 w-[20rem] h-[20rem] top-[7%] right-0 flex justify-center items-center translate-x-1/2 pointer-events-none">
+                    <canvas ref="canvas"/>
+                </div>
                 <div class="text-4xl">Peach</div>
                 <div class="flex justify-between w-[81%]">
                     <div class="uppercase pl-3">Color of the year 2024</div>
                     <div class="uppercase">Nature's soft touch</div>
                 </div>
                 <div class="text-4xl text-end">fuzz</div>
-                <div class="absolute h-full top-0 right-0 flex justify-center items-center translate-x-1/2 pointer-events-none">
-                    <canvas ref="canvas" class="scale-[.4]"/>
-                </div>
                 <Tag text="#ffbe98" class="absolute top-12 -left-5 -rotate-6"/>
                 <Tag text="peach" class="absolute bottom-[4rem] right-[7rem] rotate-6"/>
             </div>
@@ -40,6 +40,7 @@ onMounted(async () =>{
     await app.load(state.spline.scene)
     state.spline.app = app;
     state.spline.isLoaded = true;
+    canvas.value.style="width:100%; height:100%;"
 })
 </script>
 
