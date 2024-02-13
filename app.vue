ed6921262e91281c88953cfd254bf75849e6bdb6
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-02-13 19:45:13 +0300

    Opengraph

--- app.vue
+++ app.vue
@@ -5,3 +5,28 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+ useHead({
+      title: 'Peach type challenge by Tiantsoa',
+      meta: [
+          { name: 'description', content: 'Peach type challenge by Tiantsoa' }
+      ]
+  })
+
+  useSeoMeta({
+      description: 'Peach type challenge by Tiantsoa',
+      ogTitle: 'Peach Fuzz',
+      ogDescription: 'Peach type challenge by Tiantsoa',
+      ogImage: '/peach-type.jpg',
+      ogImageWidth: 540,
+      ogImageHeight: 303,
+      ogUrl: 'https://peach-type.com/',
+      twitterTitle: 'Peach Fuzz',
+      twitterDescription: 'Peach type challenge by Tiantsoa',
+      twitterImage: '/peach-type.jpg',
+      twitterCard: 'summary'
+  })
+
+
+</script>
